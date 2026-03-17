158561f9a9f76b8a1de123e4b69a447a42e59711
DomecStoces <dominik.stoces@mendelu.cz>
2026-03-17 23:46:23 +0100

    ch.

--- GAM.R
+++ GAM.R
@@ -61,7 +61,7 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-excl <- c("s(Locality)") # Exclude random effects for the smooth trend
+excl <- c("s(Locality)")
 tv <- typical_values(mod_gam2)
 
 # 1. Create the smooth sequence of 100 points
@@ -97,7 +97,7 @@ p <- ggplot() +
   geom_line(data = fv,
             aes(x = Altitude_scaled, y = fitted), linewidth = 1.1) +
   geom_jitter(data = df,
-              aes(x = Altitude_scaled, y = Trophic_scaled),
+              aes(x = Altitude_scaled, y = Size),
               width = 0.03, height = 0, size = 1.8, alpha = 0.6) +
   labs(title = "Spiders", x = "Elevational gradient (scaled)", y = "Size CWM") +
   scale_x_continuous(breaks = seq(-2, 2, 1), minor_breaks = NULL) +
@@ -122,8 +122,67 @@ p <- ggplot() +
   )
 p
 
+# vizualization for Size
+excl <- c("s(Locality)")
+tv <- typical_values(mod_gam2)
 
-tiff('GAM_Distribution.tiff', units = "in", width = 8, height = 10, res = 600)
-print(p)
-dev.off()
+# 1. Create the smooth sequence of 100 points
+alt_seq <- seq(min(df$Altitude_scaled, na.rm = TRUE),
+               max(df$Altitude_scaled, na.rm = TRUE), length.out = 100)
+
+tv2 <- dplyr::select(tv, -any_of(c("Altitude_scaled","Altitude_scaled2")))
+
+# 2. Build the prediction grid
+new_data <- tidyr::crossing(tv2, tibble(Altitude_scaled = alt_seq))
+
+# 3. Calculate fitted values USING new_data
+fv <- fitted_values(mod_gam2, data = new_data, exclude = excl,
+                    scale = "link", se = TRUE) %>%
+  dplyr::rename(fitted_link = any_of(c("fitted",".fitted","fit")),
+                se_link     = any_of(c("se",".se"))) %>%
+  mutate(
+    lower_link = fitted_link - (1.96 * se_link),
+    upper_link = fitted_link + (1.96 * se_link),
+    fitted = exp(fitted_link),  # CHANGED: exp() for log-link
+    lower  = exp(lower_link),   # CHANGED: exp() for log-link
+    upper  = exp(upper_link)    # CHANGED: exp() for log-link
+  )
+
+# 4. Plot
+p <- ggplot() +
+  # The confidence ribbon
+  geom_ribbon(data = fv,
+              aes(x = Altitude_scaled, ymin = lower, ymax = upper),
+              fill = "grey70", alpha = 0.35) +
+  
+  # The fitted line
+  geom_line(data = fv,
+            aes(x = Altitude_scaled, y = fitted), linewidth = 1.1) +
+  
+  # The points (using actual Size)
+  geom_jitter(data = df,
+              aes(x = Altitude_scaled, y = Size),
+              width = 0.03, height = 0, size = 1.8, alpha = 0.6) +
+  
+  # Labels
+  labs(title = "Spiders", x = "Elevational gradient (scaled)", y = "Size CWM") +
+  scale_x_continuous(breaks = seq(-2, 2, 1), minor_breaks = NULL) +
+  
+  # Y-Axis: CHANGED to let ggplot auto-scale to your 3-8.5 size range
+  scale_y_continuous(expand = expansion(mult = c(0.05, 0.05))) +
+  
+  theme(
+    panel.background = element_blank(),
+    plot.background  = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.line        = element_line(colour = "black", linewidth = 0.6),
+    axis.ticks       = element_line(colour = "black", linewidth = 0.5),
+    axis.ticks.length= unit(4, "pt"),
+    axis.title       = element_text(size = 15),
+    axis.text        = element_text(colour = "black", size = 11),
+    plot.margin      = margin(6, 8, 6, 6)
+  )
+
+p
 

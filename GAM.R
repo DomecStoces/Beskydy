72a8c4420439d8a3fc5d2bacbef21670ed442c0e
DomecStoces <dominik.stoces@mendelu.cz>
2026-03-17 23:37:07 +0100

    ch.

--- GAM.R
+++ GAM.R
@@ -1,53 +1,58 @@
 ### Setting model ###
+library(readxl)
 library(mgcv)
-df <- read_excel("Rao_diversity1.xlsx", sheet = "RaoQ")
-df$Altitude_scaled <- as.numeric(scale(df$Elevation, center = TRUE, scale = TRUE))
+library(stringr)
+library(dplyr)
+df <- read_excel("Beskydy_2007_2008_traits_final.xlsx", sheet = "spiders_FD")
+df$Altitude_scaled <- as.numeric(scale(df$Altitude, center = TRUE, scale = TRUE))
 df$Locality <- as.factor(df$Locality)
+df$Trees <- as.factor(df$Trees)
 df$Year <- as.factor(df$Year)
-df$Exposition2 <- as.numeric(df$Exposition2)
-df$Exposition2 <- scale(df$Exposition2)
+df <- df %>%
+  mutate(
+    Exposition2 = sapply(strsplit(as.character(Exposition), "_"), function(x) mean(as.numeric(x))),
+    Exposition2 = as.numeric(scale(Exposition2))
+  )
+head(df %>% select(Exposition, Exposition2))
 
-N <- nrow(df)
-df$Wings_cwm_scaled        <- (df$Wings_cwm * (N - 1) + 0.5) / N
-df$Dietary_cwm_scaled      <- (df$Dietary_cwm * (N - 1) + 0.5) / N
-df$Breeding_cwm_scaled     <- (df$Breeding_cwm * (N - 1) + 0.5) / N
-df$Distribution_cwm_scaled <- (df$Distribution_cwm * (N - 1) + 0.5) / N
+# Scaling traits for spiders #
+n <- nrow(df)
+df$Trophic_01 <- df$Trophic - 1
+df$Trophic_scaled <- (df$Trophic_01 * (n - 1) + 0.5) / n
+df$Dispersal_scaled <- (df$Dispersal * (n - 1) + 0.5) / n
 
-df$Year <- factor(df$Year)
-df$Locality <- factor(df$Locality)
 
 mod_gam1 <- gam(
-  Body_size_cwm ~ s(Locality, bs = "re") +
-    s(Altitude_scaled, bs = "cr",k=5) + Exposition2 +
-    s(Year, bs = "re"),
+  Size ~ s(Locality, bs = "re") +
+    s(Altitude_scaled, bs = "cr",k=3) + Exposition2 +
+    Year,
   data   = df,
   family = gaussian(link="log"),
   method = "REML"
 )
+
 mod_gam2 <- gam(
-  Trophic_cwm ~ s(Locality, bs = "re") +
-    s(Altitude_scaled, bs = "cr",k=5) + Exposition2 +
-    s(Year, bs = "re"),
+  Size ~ s(Locality, bs = "re") +
+    Altitude_scaled + Exposition2 + Site.protection + Year,
   data   = df,
-  family = betar(link="cloglog"),
+  family = gaussian(link="log"),
   method = "REML"
 )
 
-mod_gam_rao <- gam(
-  Q ~ 
+mod_gam2 <- gam(
+  Rao ~ 
     s(Locality, bs = "re") + 
-    s(Altitude_scaled, bs = "cr", k = 5)  + Exposition2 +
-    s(Year, bs = "re"),
+    Altitude_scaled  + Exposition2 + Site.protection + Year,
   data   = df,
   family = tw(link="log"), select = TRUE,
   method = "REML"
 )
 
-summary(mod_gam1)
+summary(mod_gam2)
 par(mfrow = c(2, 2))
-gam.check(mod_gam1)
-concurvity(mod_gam1, full = TRUE)
-gratia::draw(mod_gam1)
+gam.check(mod_gam2)
+concurvity(mod_gam2, full = TRUE)
+gratia::draw(mod_gam2)
 plot(mod_gam1, select = 2)
 
 ### Plotting the effect of Altitude_scaled on CWM traits ###
@@ -56,59 +61,50 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-excl <- c("s(Locality)", "s(Year)")
-tv <- typical_values(mod_gam1)
+excl <- c("s(Locality)") # Exclude random effects for the smooth trend
+tv <- typical_values(mod_gam2)
 
+# 1. Create the smooth sequence of 100 points
 alt_seq <- seq(min(df$Altitude_scaled, na.rm = TRUE),
                max(df$Altitude_scaled, na.rm = TRUE), length.out = 100)
 
 tv2 <- dplyr::select(tv, -any_of(c("Altitude_scaled","Altitude_scaled2")))
 
-new_data <- tidyr::crossing(tv2, tibble(Altitude_scaled = alt_seq)) %>%
-  mutate(Altitude_scaled2 = Altitude_scaled^2)
-new_data <- tidyr::crossing(tv2, tibble(Altitude_scaled = alt_seq)) %>%
-  mutate(Altitude_scaled2 = Altitude_scaled^2)
-inv_link <- family(mod_gam1)$linkinv
-fv <- fitted_values(mod_gam1, data = new_data, exclude = excl,
+# 2. Build the prediction grid
+new_data <- tidyr::crossing(tv2, tibble(Altitude_scaled = alt_seq))
+
+# 3. Calculate fitted values USING new_data (not df!)
+fv <- fitted_values(mod_gam2, data = new_data, exclude = excl,
                     scale = "link", se = TRUE) %>%
   dplyr::rename(fitted_link = any_of(c("fitted",".fitted","fit")),
                 se_link     = any_of(c("se",".se"))) %>%
   mutate(
     lower_link = fitted_link - (1.96 * se_link),
     upper_link = fitted_link + (1.96 * se_link),
-    fitted = inv_link(fitted_link),
-    lower  = inv_link(lower_link),
-    upper  = inv_link(upper_link)
+    fitted = plogis(fitted_link),  
+    lower  = plogis(lower_link),   
+    upper  = plogis(upper_link)    
   )
-fv_obs <- fitted_values(mod_gam1, data = df, exclude = excl,
-                        scale = "response", se = FALSE) %>%
-  dplyr::rename(fitted = any_of(c("fitted",".fitted","fit")))
 
-df$partial_excl <- residuals(mod_gam1, type = "response") + fv_obs$fitted
-
+# 4. Plot
 p <- ggplot() +
-  # 1. The confidence ribbon
+  # The confidence ribbon (now smooth!)
   geom_ribbon(data = fv,
               aes(x = Altitude_scaled, ymin = lower, ymax = upper),
               fill = "grey70", alpha = 0.35) +
   
-  # 2. The fitted line (the "estimated curve")
+  # The fitted line (now smooth!)
   geom_line(data = fv,
             aes(x = Altitude_scaled, y = fitted), linewidth = 1.1) +
-  
-  # 3. The points (using your dispersal column)
   geom_jitter(data = df,
-              aes(x = Altitude_scaled, y = Wings_cwm_scaled),
+              aes(x = Altitude_scaled, y = Trophic_scaled),
               width = 0.03, height = 0, size = 1.8, alpha = 0.6) +
-  
-  # Labels
-  labs(x = "Elevational gradient (scaled)", y = "Dispersal ability CWM") +
-  
-  # X-Axis: Matches the Rao's Q plot limits exactly
+  labs(title = "Spiders", x = "Elevational gradient (scaled)", y = "Size CWM") +
   scale_x_continuous(breaks = seq(-2, 2, 1), minor_breaks = NULL) +
-  # Y-Axis: FORCED TO 0 - 1
+  
+  # Y-Axis strictly 0 to 1
   scale_y_continuous(
-    limits = c(0, 1),             
+    limits = c(0, 1),              
     breaks = seq(0, 1, 0.2),         
     expand = expansion(mult = c(0, 0.02)) 
   ) +
@@ -125,6 +121,9 @@ p <- ggplot() +
     plot.margin      = margin(6, 8, 6, 6)
   )
 p
+
+
 tiff('GAM_Distribution.tiff', units = "in", width = 8, height = 10, res = 600)
 print(p)
 dev.off()
+

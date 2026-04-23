c15c196e9459fbdd7409051a52687aca98ce94f8
DomecStoces <dominik.stoces@mendelu.cz>
2026-04-23 17:22:32 +0200

    ch.

--- Merging_taxa_env.R
+++ Merging_taxa_env.R
@@ -19,7 +19,7 @@ taxa_sheets <- list(
 fd_vars <- c("Size", "Trophic", "Dispersal", "Rao", 
              "Count", "Richness", "SESpd", "MPD")
 
-env_vars <- c("Locality", "Year", "Date", "Time.period", 
+env_vars <- c("Locality", "Year", "Date", "Time.period", "Exposition", 
               "Temperature", "Precipitation", 
               "Site.protection", "Altitude", "Trees")
 

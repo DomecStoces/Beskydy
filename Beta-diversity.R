4028461c4ba33ba218907495b094453b6b5d284a
DomecStoces <dominik.stoces@mendelu.cz>
2026-04-24 21:20:08 +0200

    deleted this pipeline approach due to ambiguity. Left beta-diversity for overall test of arthropod composition of dataset CANOCO_FINAL

--- Beta-diversity.R
+++ Beta-diversity.R
@@ -1,10 +1,3 @@
-# Number of species for each row # 
-library(readxl)
-library(writexl)
-df <- read_excel("Beskydy_2007_2008_traits_final.xlsx", sheet = "spiders_compo_names")
-df$species_richness <- rowSums(df[, -1] > 0)
-write_xlsx(df[, c("ID", "species_richness")], "species_richness.xlsx")
-
 library(vegan)
 library(dplyr)
 library(tidyr)
